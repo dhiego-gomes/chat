175e7ca31183c091edd1771253fba41ea072d319
Dhiego Gomes <dhiego.jw@gmail.com>
2021-06-12 11:54:03 -0300

    :rocket: Envia função 'autoRolar' e melhorias de design

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,21 @@
 document.querySelector('.messages').addEventListener('scroll', function() { 
-    if ((this.scrollTop + this.offsetHeight) > this.scrollHeight) {
+    console.log("ScrollTop: ", this.scrollTop);
+    console.log("OffsetHeight: ", this.offsetHeight);
+    console.log("ScrollHeight: ", this.scrollHeight);
+    console.log("Total: ", this.scrollTop + this.offsetHeight);
+    if ((this.scrollTop + this.offsetHeight) >= this.scrollHeight) {
         document.querySelector('#descer').style.display = 'none'
+        console.log('final')
     } else {
         document.querySelector('#descer').style.display = 'inline'
     }
 })
+
+function showButton() {
+    var input = document.querySelector('#message_content')
+    if (input.value == '') {
+        document.querySelector('#send').style.display = 'none'
+    } else {
+        document.querySelector('#send').style.display = 'inline'
+    }
+}
